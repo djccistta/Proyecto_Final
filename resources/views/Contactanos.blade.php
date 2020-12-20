38bd1859215bd17b525a25dfcd878b7592f34337
djccistta <dcancinas.181ci07@istta.edu.pe>
2020-12-20 18:58:42 -0500

    vistas

--- resources/views/Contactanos.blade.php
+++ resources/views/Contactanos.blade.php
@@ -8,38 +8,41 @@
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
+        <link rel="stylesheet" href="css/estilos.css">
     </head>
     <body >
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container">
-                <a class="navbar-brand" href="#"> <b>Logooo</b> </a>
+                <a class="navbar-brand" href="#"> 
+                    <img src="https://blog.elinsignia.com/wp-content/uploads/2019/06/viajes-jairan-1.png" height="70px" />
+                </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
+                    <ul class="navbar-nav mr-auto ">
                         <li class="nav-item">
-                            <a class="nav-link" href="/">Inicio</a>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link " href="{{route('Paquete.listaInicio')}}">Paquetes</a>
+                            <a class="nav-link text-dark letra-nav" href="/"> <b>Inicio</b> </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/nosotros">Nosotros</a>
+                            <a class="nav-link text-dark  letra-nav" href="{{route('Paquete.listaInicio')}}"> <b>Paquetes</b> </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/contactanos">Contactanos</a>
+                            <a class="nav-link text-dark letra-nav" href="/nosotros"><b>Nosotros</b></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-dark  letra-nav" href="/contactanos"><b>Contactanos</b></a>
                         </li>
                         
                         @if (Route::has('login'))
                             @auth
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ url('/home') }}">Principal</a>
+                                <a class="nav-link text-dark  letra-nav" href="{{ url('/home') }}"><b>Principal</b></a>
                             </li>
                             @else
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">Ingresar</a>
+                                <a class="nav-link text-dark  letra-nav" href="{{ route('login') }}"><b>Ingresar</b></a>
                             </li>
                             @endauth
                         @endif
@@ -48,11 +51,11 @@
             </div>
         </nav>
 
-        <div class="container mb-3 mt-3">
+        <div class="container mb-5 mt-5">
 
             <div class="row">
                 <div class="col-md-12">
-                    <h1>Contáctenos</h1>
+                    <h1 class="texto">Contáctenos</h1>
                 </div>
             </div>
 
@@ -88,30 +91,41 @@
         <footer class="bg-light">
             <div class="container">
                 <nav class="row">
-                    <div class="col-md-6">
-                        <h2>Escribenos:</h2>
-                        <li>
-                            <a href="https://api.whatsapp.com/send?phone=+51965131044" target="_blank"><ion-icon name="logo-whatsapp"></ion-icon>+51 965 131 044</a>
-                        </li>
-                        <li>
-                        <a href="https://api.whatsapp.com/send?phone=+51934996407" target="_blank"><ion-icon name="logo-whatsapp"></ion-icon>+51 934 996 407</a>
-                        </li>
-                        <li>
-                        <a href="https://api.whatsapp.com/send?phone=+51974254763" target="_blank"><ion-icon name="logo-whatsapp"></ion-icon>+51 974 254 763</a>
-                        </li>
+                    <div class="col-md-3 mt-3 ">
+
                     </div>
                     <div class="col-md-3">
-                        <a class="nav-link text-dark" href="/">Inicio</a>
-                        <a class="nav-link text-dark" href="{{route('Paquete.listaInicio')}}">Paquetes</a>
-                        <a class="nav-link text-dark" href="/contactanos">Contactanos</a>
-                        <a class="nav-link text-dark" href="/nosotros">Nosotros</a>
+                        <br>
+                        <h2 class="text-dark">Escribenos:</h2>
+                            <ion-icon name="logo-whatsapp" class="text-success icono"></ion-icon>
+                            <a class="text-dark" href="https://api.whatsapp.com/send?phone=+51965131044" target="_blank"><b>+51 965 131 044</b></a>
+                        <br>
+                            <ion-icon name="logo-whatsapp" class="text-success icono"></ion-icon>
+                            <a class="text-dark" href="https://api.whatsapp.com/send?phone=+51934996407" target="_blank"><b>+51 934 996 407</b></a>
+                        <br>
+                            <ion-icon name="logo-whatsapp" class="text-success icono"></ion-icon>
+                            <a class="text-dark" href="https://api.whatsapp.com/send?phone=+51974254763" target="_blank"><b>+51 974 254 763</b></a>
+                    </div>
+                    <div class="col-md-3 mt-3">
+                        <br>
+                        <a class="text-dark icono" href="/"><ion-icon name="home-outline"></ion-icon> <b>Inicio</b></a>
+                        <br>
+                        <a class="text-dark icono" href="{{route('Paquete.listaInicio')}}"><ion-icon name="location-outline"></ion-icon><b>Paquetes</b></a>
+                        <br>
+                        <a class="text-dark icono" href="/nosotros"><ion-icon name="people-outline"></ion-icon><b>Nosotros</b></a>
+                        <br>
+                        <a class="text-dark icono" href="/contactanos"><ion-icon name="call-outline"></ion-icon><b>Contactanos</b></a>
+                        
                    </div>
-                    <div class="col-md-3">
-                            logoooooo
+                    <div class="col-md-3 mt-3 ">
+                        <img src="https://blog.elinsignia.com/wp-content/uploads/2019/06/viajes-jairan-1.png" height="150px" />
                     </div>
+                    
                 </nav>
             </div>
         </footer>
+
+
 
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
